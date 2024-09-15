9e28530c85a15df15f3f80318bf59403713d39af
arthur <wr80340@smail.nchu.edu.tw>
2024-09-15 20:21:13 +0800

    #add f value

--- ML_MODELS/LinearModels/LinearModel.py
+++ ML_MODELS/LinearModels/LinearModel.py
@@ -2,14 +2,13 @@
 from typing import List, Optional
 
 import numpy as np
-import pandas as pd
 import plotly.graph_objects as go
-from scipy.stats import t
+from scipy.stats import f, t
 
 
 # %%
 class LinearModel:
-    def __init__(self, intercept: bool = True):
+    def __init__(self, intercept: bool = True) -> None:
         self.intercept = intercept
         self.fitted = False
 
@@ -24,6 +23,9 @@ class LinearModel:
         self.feature_names = (
             f_names if f_names is not None else [f"f{i}" for i in range(x.shape[1])]
         )
+        self.n, self.p = x.shape
+        self.deg_of_freedom = self.n - self.p - 1
+
         self.model_matrix = self.__add_intercept(x)
         self.y = y
 
@@ -36,14 +38,12 @@ class LinearModel:
         xtx_inv = np.linalg.pinv(self.model_matrix.T @ self.model_matrix)
         self.beta_hat = xtx_inv @ self.model_matrix.T @ self.y
         self.residuals = self.y - self.model_matrix @ self.beta_hat
-        self.regression_dum_of_square = np.sum(self.residuals**2)
-        self.deg_of_freedom = (
-            self.model_matrix.shape[0] - self.model_matrix.shape[1]
-        )  # n - p
-        self.sigma_hat = np.sqrt(self.regression_dum_of_square / self.deg_of_freedom)
 
-        # Covariance matrix
+        self.reg_sum_of_square = np.sum(self.residuals**2)
+
+        self.sigma_hat = np.sqrt(self.reg_sum_of_square / self.deg_of_freedom)
         self.cov_mat = xtx_inv * np.pow(self.sigma_hat, 2)
+
         self.fitted = True
 
     def plot_residual(self) -> None:
@@ -65,34 +65,47 @@ class LinearModel:
         )
         fig.show()
 
-    def summary(self) -> pd.DataFrame:
+    def summary(self) -> dict:
         if not self.fitted:
             raise ValueError(
                 "The model has not been fitted yet. Call the fit method first."
             )
+        self.null_reg_sum_of_square = np.pow(self.y - self.y.mean(), 2).sum()
 
-        summary_table_idx = (
-            ["intercept"] + self.feature_names if self.intercept else self.feature_names
+        self.r_square = 1.0 - self.reg_sum_of_square / self.null_reg_sum_of_square
+
+        self.adj_r_square = 1.0 - (
+            (1 - self.r_square) * (self.n - 1) / self.deg_of_freedom
         )
-        test_statistic = self.beta_hat / np.sqrt(np.diag(self.cov_mat))
-        p_values = [
+
+        t_statistic = self.beta_hat / np.sqrt(np.diag(self.cov_mat))
+        t_p_values = [
             2
             * min(
                 1 - t.cdf(abs(t_stat), self.deg_of_freedom),
                 t.cdf(abs(t_stat), self.deg_of_freedom),
             )
-            for t_stat in test_statistic
+            for t_stat in t_statistic
         ]
-
-        return pd.DataFrame(
-            {
-                "beta": self.beta_hat,
-                "se(beta)": np.sqrt(np.diag(self.cov_mat)),
-                "test_statistic": test_statistic,
-                "p_value": p_values,
-            },
-            index=summary_table_idx,
+        self.f_statistic = (
+            (self.null_reg_sum_of_square - self.reg_sum_of_square)
+            / self.p
+            / self.reg_sum_of_square
+            * (self.n - self.p - 1)
         )
+        self.f_p_value = 1 - f.cdf(
+            self.f_statistic, dfd=self.n - self.p - 1, dfn=self.p
+        )
+
+        row_idx = (
+            ["intercept"] + self.feature_names if self.intercept else self.feature_names
+        )
+        return {
+            "beta": {k: b for k, b in zip(row_idx, self.beta_hat)},
+            "se(beta)": {k: b for k, b in zip(row_idx, np.sqrt(np.diag(self.cov_mat)))},
+            "test_statistic": {k: b for k, b in zip(row_idx, t_statistic)},
+            "p_value": {k: b for k, b in zip(row_idx, t_p_values)},
+        }
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         if not self.fitted:
@@ -103,8 +116,13 @@ class LinearModel:
         x = self.__add_intercept(x)
         return x @ self.beta_hat
 
+    def lm_report(self):
+        pass
+
 
 # %%
+import pandas as pd
+
 iris = pd.read_csv("iris.csv")
 # %%
 lm = LinearModel()
@@ -115,6 +133,17 @@ lm.fit(
 )
 lm.plot_residual()
 # %%
-lm.summary()
+pd.DataFrame.from_dict(lm.summary(), orient="columns")
 
+# %%
+lm.r_square
+# %%
+lm.adj_r_square
+# %%
+lm.p
+
+# %%
+lm.f_statistic
+# %%
+lm.f_p_value
 # %%
