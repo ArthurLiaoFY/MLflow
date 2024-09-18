f117029cb85c8b4623c2e6cc018dc4e29208da6f
arthur <wr80340@smail.nchu.edu.tw>
2024-09-18 17:18:40 +0800

    #remove train test split in eda

--- run_curve_classify_eda.py
+++ run_curve_classify_eda.py
@@ -28,19 +28,3 @@ plot_curve(
     label=label,
     plot_file_path=config["curve_classify"]["plot_file_path"],
 )
-
-# %%
-train_idx, val_idx = train_test_split(
-    label.index,
-    train_size=0.8,
-    random_state=1122,
-    shuffle=True,
-    stratify=label["test_result"],
-)
-
-# %%
-train_x = cum_curve.iloc[train_idx, :]
-val_x = cum_curve.iloc[val_idx, :]
-train_y = label.iloc[train_idx, :]
-val_y = label.iloc[val_idx, :]
-# %%
