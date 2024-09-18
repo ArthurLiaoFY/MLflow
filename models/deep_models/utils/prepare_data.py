6c0d1bc81ed5859cd5923886cd95026294ba9ac4
arthur <wr80340@smail.nchu.edu.tw>
2024-09-18 13:49:37 +0800

    #formatting

--- models/deep_models/utils/prepare_data.py
+++ models/deep_models/utils/prepare_data.py
@@ -1,14 +1,17 @@
+from collections.abc import Iterable
+
 import numpy as np
 import pandas as pd
 import torch
 from torch.nn.functional import one_hot
-from torch.utils.data import Dataset, DataLoader, TensorDataset
-from collections.abc import Iterable
+from torch.utils.data import DataLoader, Dataset, Sampler, TensorDataset
+
 from models.deep_models.utils.tools import get_device
-from torch.utils.data import Sampler
 
 
-def to_tensor(x: np.ndarray | pd.DataFrame | torch.Tensor, device: str | None = None) -> torch.Tensor | None:
+def to_tensor(
+    x: np.ndarray | pd.DataFrame | torch.Tensor, device: str | None = None
+) -> torch.Tensor | None:
     device = get_device() if device is None else device
     match type(x):
         case np.ndarray:
@@ -23,7 +26,9 @@ def to_tensor(x: np.ndarray | pd.DataFrame | torch.Tensor, device: str | None = 
             try:
                 tensor = torch.tensor(x).to(torch.float).to(device)
             except:
-                print("X variable must be of one of the types: np.ndarray, pd.DataFrame, torch.Tensor.")
+                print(
+                    "X variable must be of one of the types: np.ndarray, pd.DataFrame, torch.Tensor."
+                )
                 tensor = None
 
     return tensor
@@ -36,12 +41,19 @@ def to_dataset(*data: np.ndarray | pd.DataFrame | torch.Tensor) -> TensorDataset
 
 
 def to_dataloader(
-        *data: np.ndarray | pd.DataFrame | torch.Tensor,
-        batch_size: int = 32,
-        sampler: Sampler | None = None,
-        shuffle: bool = True) -> DataLoader:
+    *data: np.ndarray | pd.DataFrame | torch.Tensor,
+    batch_size: int = 32,
+    sampler: Sampler | None = None,
+    shuffle: bool = True,
+) -> DataLoader:
 
-    return DataLoader(to_dataset(*data), batch_size=batch_size, shuffle=shuffle if sampler is None else False, drop_last=True)
+    return DataLoader(
+        to_dataset(*data),
+        batch_size=batch_size,
+        shuffle=shuffle if sampler is None else False,
+        drop_last=True,
+    )
+
 
 def one_hot_encoding(x: np.ndarray | pd.DataFrame | torch.Tensor) -> torch.Tensor:
     match type(x):
