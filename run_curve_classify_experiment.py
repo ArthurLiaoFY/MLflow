d32e0724234f637e3556b2d7fc969d7b2aa80861
arthur <wr80340@smail.nchu.edu.tw>
2024-09-18 14:09:48 +0800

    #add train module

--- run_curve_classify_experiment.py
+++ run_curve_classify_experiment.py
@@ -6,7 +6,11 @@ from sklearn.model_selection import train_test_split
 
 from models.deep_models.models.conv_gru_att import Convolutional1DGRUAttention
 from models.deep_models.utils.prepare_data import to_dataloader
-from models.deep_models.utils.tools import check_model_device, get_device
+from models.deep_models.utils.tools import (
+    check_model_device,
+    check_tensor_device,
+    get_device,
+)
 from projects.curve_classify.load_data import load_data
 
 # %%
@@ -24,28 +28,26 @@ train_idx, val_idx = train_test_split(
     shuffle=True,
     stratify=label["test_result"],
 )
-
 # %%
-train_x = cum_curve.iloc[train_idx, :]
-val_x = cum_curve.iloc[val_idx, :]
-train_y = label.iloc[train_idx, :]
-val_y = label.iloc[val_idx, :]
+train_loader = to_dataloader(
+    cum_curve.iloc[train_idx, :].to_numpy(),
+    label.loc[train_idx, "test_result"].to_numpy(),
+)
+valid_loader = to_dataloader(
+    cum_curve.iloc[val_idx, :].to_numpy(),
+    label.loc[val_idx, "test_result"].to_numpy(),
+)
 # %%
 device = get_device()
 conv_gru_att = Convolutional1DGRUAttention(
-    conv_in_channels=train_x.shape[1],
-    conv_out_channels=128,
-    gru_input_size=128,
-    gru_hidden_size=128,
+    conv_in_channels=1,  # C in shape : (B, C, H)
+    conv_out_channels=32,  # C in shape : (B, C, H)
+    gru_input_size=cum_curve.shape[1],
+    gru_hidden_size=64,
     gru_layer_amount=2,
+    attention_num_of_head=8,
     out_feature_size=2,
 ).to(device=device)
 # %%
 
-x = torch.randn(size=(64, train_x.shape[1])).to(device=device)
-x.shape
-# %%
-check_model_device(model=conv_gru_att)
-# %%
-conv_gru_att.forward(input_tensor=x)
 # %%
