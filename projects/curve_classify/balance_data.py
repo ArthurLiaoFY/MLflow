6b71be2ae00e3255956083fb706c9bb3fb03e2d3
arthur <wr80340@smail.nchu.edu.tw>
2024-09-21 11:39:45 +0800

    #add smote

--- projects/curve_classify/balance_data.py
+++ projects/curve_classify/balance_data.py
@@ -1,18 +1,36 @@
 import numpy as np
 
 
-def functional_smote(
-    curve_array: np.ndarray,
-    label_array: np.ndarray,
+def smote(
+    curve_array: np.ndarray, label_array: np.ndarray, seed: int = 1122
 ) -> tuple[np.ndarray, np.ndarray]:
+    rand_seed = np.random.RandomState(seed)
 
-    pass
+    unique_labels, counts = np.unique(label_array, return_counts=True)
+    min_label = unique_labels[np.argmin(counts)]
+    min_class_indices = np.where(label_array == min_label)[0]
+
+    num_to_generate = np.max(counts) - np.min(counts)
+
+    gen_curve_array = np.zeros((num_to_generate, curve_array.shape[1]))
+    gen_label_array = np.full(num_to_generate, min_label)
+
+    for i in range(num_to_generate):
+        idx_1, idx_2 = rand_seed.choice(min_class_indices, size=2, replace=False)
+        alpha = rand_seed.uniform(low=0.0, high=1.0, size=1)
+
+        gen_curve_array[i] = curve_array[idx_1] + alpha * (
+            curve_array[idx_2] - curve_array[idx_1]
+        )
+
+    return (
+        np.vstack([curve_array, gen_curve_array]),
+        np.hstack([label_array, gen_label_array]),
+    )
 
 
 def up_sampling(
-    curve_array: np.ndarray,
-    label_array: np.ndarray,
-    seed: int = 1122,
+    curve_array: np.ndarray, label_array: np.ndarray, seed: int = 1122
 ) -> tuple[np.ndarray, np.ndarray]:
     unique_labels, counts = np.unique(label_array, return_counts=True)
 
@@ -21,7 +39,9 @@ def up_sampling(
 
     n_samples_to_add = np.max(counts) - np.min(counts)
 
-    dup_indices = np.random.RandomState(seed).choice(min_class_indices, size=n_samples_to_add, replace=True)
+    dup_indices = np.random.RandomState(seed).choice(
+        min_class_indices, size=n_samples_to_add, replace=True
+    )
 
     return (
         np.concatenate([curve_array, curve_array[dup_indices]], axis=0),
