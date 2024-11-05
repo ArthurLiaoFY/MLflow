2da893ab15f117afb7cdd019d77070da2d27a5a5
arthur <wr80340@smail.nchu.edu.tw>
2024-11-05 11:18:13 +0800

    #change data source

--- run_opt_ml_taichung_housing.py
+++ run_opt_ml_taichung_housing.py
@@ -1,22 +1,53 @@
 # %%
+import os
 from configparser import ConfigParser
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from xgboost import XGBRegressor
 
 from projects.opt_ml.estimate_surface import EstimateSurface
 from projects.opt_ml.optimize_response import optimize_f_hat
 from projects.opt_ml.plot_fns import plot_obj_surface
 
+# load config
 config = ConfigParser()
 config.read("projects/opt_ml/opt_ml.ini")
-config = config["boston_housing"]
+config = config["taichung_housing"]
+
+# load data
+presale_data_list = []
+secondhand_data_list = []
+for path, root, files in os.walk("./data/taichung_housing"):
+    for f in files:
+        if f.endswith("lvr_land_A.csv"):
+            secondhand_data_list.append(os.path.join(path, f))
+        if f.endswith("lvr_land_B.csv"):
+            presale_data_list.append(os.path.join(path, f))
 
 
-train_df = pd.read_csv("./data/boston_housing/train.csv")
-train_df = train_df.loc[train_df["2ndFlrSF"] > 0, :]
+presale_df = pd.concat(
+    objs=(pd.read_csv(f, header=[0, 1]) for f in presale_data_list),
+    axis=0,
+)
+presale_df(('預售中古', '_')) = '預售屋'
+secondhand_df = pd.concat(
+    objs=(pd.read_csv(f, header=[0, 1]) for f in secondhand_data_list),
+    axis=0,
+)
+secondhand_df(('預售中古', '_')) = '中古屋'
+# %%
+presale_df_columns_map = {
+    'c_'+str(idx): org_col[0]
+    for idx, org_col in enumerate(presale_df.columns)
+}
+
+
+secondhand_df_columns_map = {
+    'c_'+str(idx): org_col[0]
+    for idx, org_col in enumerate(secondhand_df.columns)
+}
+
 
 # %%
 x_min = [
@@ -29,37 +60,6 @@ x_max = [
     train_df["2ndFlrSF"].quantile(float(config.get("upper_quantile"))),
 ]
 # %%
-# plt.plot(train_df["1stFlrSF"], train_df["2ndFlrSF"], "o")
-# plt.vlines(
-#     x=train_df["1stFlrSF"].quantile(float(config.get("lower_quantile"))),
-#     ymin=x_min[1],
-#     ymax=x_max[1],
-#     colors="red",
-#     linestyles="--",
-# )
-# plt.vlines(
-#     x=train_df["1stFlrSF"].quantile(float(config.get("upper_quantile"))),
-#     ymin=x_min[1],
-#     ymax=x_max[1],
-#     colors="red",
-#     linestyles="--",
-# )
-
-# plt.hlines(
-#     y=train_df["2ndFlrSF"].quantile(float(config.get("lower_quantile"))),
-#     xmin=x_min[0],
-#     xmax=x_max[0],
-#     colors="red",
-#     linestyles="--",
-# )
-# plt.hlines(
-#     y=train_df["2ndFlrSF"].quantile(float(config.get("upper_quantile"))),
-#     xmin=x_min[0],
-#     xmax=x_max[0],
-#     colors="red",
-#     linestyles="--",
-# )
-# plt.show()
 
 # %%
 
