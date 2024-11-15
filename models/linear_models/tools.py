d8415e7cf748e42f87d52c346e8ac74a8d6a7c8d
arthur <wr80340@smail.nchu.edu.tw>
2024-11-15 15:09:07 +0800

    #bug fix

--- models/linear_models/tools.py
+++ models/linear_models/tools.py
@@ -5,7 +5,7 @@ def to_model_matrix(x: np.ndarray) -> np.ndarray:
     x = x if x.ndim == 2 else x[:, np.newaxis]
     return np.concatenate(
         (
-            np.ones_like(x),
+            np.ones(shape=(x.shape[0], 1)),
             x,
         ),
         axis=1,
