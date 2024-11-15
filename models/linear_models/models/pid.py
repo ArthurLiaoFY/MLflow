68c8266af554bfa0718e67521f3ae91ce564ec84
arthur <wr80340@smail.nchu.edu.tw>
2024-11-15 11:36:13 +0800

    #modify structure

--- models/linear_models/models/pid.py
+++ models/linear_models/models/pid.py
@@ -3,27 +3,19 @@ import numpy as np
 
 
 class PIDController:
-    def __init__(self, target_point: float):
-        self.Kp = None
+    def __init__(
+        self,
+        target_point: float,
+    ):
         self.epsilon = 1e-5
         self.target_point = target_point
 
-    def compute(self, y_new: float):
-        return self.Kp * (self.target_point - y_new)
+    def compute(self, y_new: float, Kp: np.ndarray):
+        return Kp * (self.target_point - y_new)
 
-    def update_Kp(self, X: np.ndarray, y: np.ndarray) -> None:
-        if self.Kp is None:
-            self.Kp = np.ones(shape=(X.shape[-1] - 1))
+    def estimate_Kp(self, X: np.ndarray, y: np.ndarray) -> None:
         beta_hat = np.linalg.inv(X.T @ X) @ X.T @ y
-
-        self.Kp = np.max(
-            np.concatenate(
-                (
-                    1 / (beta_hat + self.epsilon),
-                    np.zeros_like(beta_hat),
-                ),
-                axis=0,
-            ),
-            axis=0,
-        )
-
+        return np.maximum(
+            1 / (beta_hat + self.epsilon),
+            np.zeros_like(beta_hat),
+        )[1:, :]
