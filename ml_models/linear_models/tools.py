27cfd3f44581e6ccec7aa8d1620c2ac2c4dfae0e
arthur <wr80340@smail.nchu.edu.tw>
2024-11-19 09:52:44 +0800

    #use X instead of x, add local poly method

--- ml_models/linear_models/tools.py
+++ ml_models/linear_models/tools.py
@@ -1,12 +1,31 @@
 import numpy as np
 
 
-def to_model_matrix(x: np.ndarray) -> np.ndarray:
-    x = x if x.ndim == 2 else x[:, np.newaxis]
+def to_model_matrix(X: np.ndarray) -> np.ndarray:
+    X = X if X.ndim == 2 else X[:, np.newaxis]
     return np.concatenate(
         (
-            np.ones(shape=(x.shape[0], 1)),
-            x,
+            np.ones(shape=(X.shape[0], 1)),
+            X,
         ),
         axis=1,
+    )
+
+
+def to_local_constant_model_matrix(X: np.ndarray) -> np.ndarray:
+    return np.ones(shape=(X.shape[0], 1))
+
+
+def to_local_linear_model_matrix(X: np.ndarray, knot: float) -> np.ndarray:
+    relative_distance = X - knot if X.ndim == 2 else X[:, np.newaxis] - knot
+    return to_model_matrix(X=relative_distance)
+
+
+def to_local_polynomial_model_matrix(X: np.ndarray, knot: float) -> np.ndarray:
+    relative_distance = X - knot if X.ndim == 2 else X[:, np.newaxis] - knot
+    return to_model_matrix(
+        X=np.concatenate(
+            (relative_distance, relative_distance**2),
+            axis=1,
+        )
     )
