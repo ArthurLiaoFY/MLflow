8ea65aed663efa9429cc3cdba489cbef855d7709
arthur <wr80340@smail.nchu.edu.tw>
2024-11-21 11:40:32 +0800

    #remove learning rate

--- ml_models/linear_models/models/pid.py
+++ ml_models/linear_models/models/pid.py
@@ -6,25 +6,19 @@ class PIDController:
     def __init__(
         self,
         target_point: float,
-        learning_rate: float = 1,
-        learning_rate_decay: float = 0.999,
         epsilon: float = 1e-7,
     ):
         self.reset_target_point(
             target_point=target_point,
-            learning_rate=learning_rate,
         )
-        self.learning_rate_decay = learning_rate_decay
         self.epsilon = epsilon
 
-    def reset_target_point(self, target_point: float, learning_rate: float):
+    def reset_target_point(self, target_point: float):
         self.target_point = target_point
-        self.learning_rate = learning_rate
 
     def compute(self, y_new: float, Kp: np.ndarray):
         return Kp * (self.target_point - y_new)
 
     def estimate_Kp(self, X: np.ndarray, y: np.ndarray) -> None:
         beta_hat = np.linalg.pinv(X.T @ X) @ X.T @ y
-        self.learning_rate *= self.learning_rate_decay
-        return self.learning_rate / (beta_hat + self.epsilon)[1:, :]
+        return 1 / (beta_hat + self.epsilon)[1:, :]
