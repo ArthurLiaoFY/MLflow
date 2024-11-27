e4f2a59f70c34e59fbcccde22eb43ef06578c8a3
arthur <wr80340@smail.nchu.edu.tw>
2024-11-27 11:48:03 +0800

    #consider degree of freedom

--- ml_models/descriptive_statistic.py
+++ ml_models/descriptive_statistic.py
@@ -6,8 +6,8 @@ def sample_mean(X: np.ndarray):
 
 
 def sample_variance(X: np.ndarray):
-    return (X - sample_mean(X).T) @ (X - sample_mean(X).T) / (len(X) - 1)
+    return (X - sample_mean(X)).T @ (X - sample_mean(X)) / (len(X) - 1)
 
 
 def sample_covariance(X1: np.ndarray, X2: np.ndarray):
-    return (X1 - sample_mean(X1).T) @ (X2 - sample_mean(X2).T) / (len(X1) - 1)
+    return (X1 - sample_mean(X1)).T @ (X2 - sample_mean(X2)) / (len(X1) - 1)
